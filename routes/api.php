6ac3a2dc3f73933057d5d4ed1d5854e7e860cae5
Hugo Marques <hugo.marques62@gmail.com>
2021-01-16 16:43:34 -0300

    update

--- routes/api.php
+++ routes/api.php
@@ -13,19 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::group(['middleware' => 'jwt'], function () {
-    Route::group([
-        'middleware' => 'api',
-    ], function ($router) {
-        Route::prefix('users')->group(function () {
-            Route::post('/login', 'UserController@login');
-            Route::get('/view/{id}', 'UserController@view');
-            Route::get('/me', 'UserController@me');
+Route::group([
+    'middleware' => 'api',
+], function ($router) {
+    Route::prefix('users')->group(function () {
+        Route::post('/login', 'UserController@login');
+        Route::get('/view/{id}', 'UserController@view');
+        Route::get('/me', 'UserController@me');
 
-            Route::get('/logout', 'UserController@logout');
-            Route::post('/create', 'UserController@store');
-            Route::delete('/delete/{id}', 'UserController@delete');
-            Route::put('/update/{id}', 'UserController@update');
-        });
+        Route::get('/logout', 'UserController@logout');
+        Route::post('/create', 'UserController@store');
+        Route::delete('/delete/{id}', 'UserController@delete');
+        Route::put('/update/{id}', 'UserController@update');
     });
 });

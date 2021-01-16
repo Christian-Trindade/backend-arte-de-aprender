914e4250b4b447e568bd543673eb85d31c0f1a47
Hugo Marques <hugo.marques62@gmail.com>
2021-01-16 19:07:46 -0300

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -61,7 +61,7 @@ class UserController extends Controller
             $user = User::create($data);
             !$token = auth()->attempt($validator->validated());
                
-                $this->createNewToken($token);
+               return $this->createNewToken($token);
                
         // } catch (\Throwable $th) {
             //throw $th;

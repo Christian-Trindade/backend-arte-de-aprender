99d28aa830c13c4222942b94c81cece3aa75619d
Ubuntu <ubuntu@ip-172-31-14-137.us-east-2.compute.internal>
2021-01-16 16:56:05 +0000

    routes test

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/login', 'UserController@login');
-
+Route::post('/create-user', 'UserController@store');
 Route::group(['middleware' => 'jwt'], function () {
     Route::prefix('users')->group(function () {
         Route::post('/create', 'UserController@store');

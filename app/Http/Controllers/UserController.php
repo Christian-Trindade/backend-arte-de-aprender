e1821297e8452e0696592af9d2a0956177e8edb0
Hugo Marques <hugo.marques62@gmail.com>
2021-01-16 22:29:40 -0300

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,7 +42,7 @@ class UserController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            'expires_in' => auth('api')->factory()->getTTL() * 6000,
             'user' => auth()->user(),
         ]);
     }
